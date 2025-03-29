0fbc966a4b5126681b5842854cc25b40a4180fcf
vidall28 <vidalkaique.az@gmail.com>
2025-03-29 01:45:55 -0300

    Delete verdadeira GP TROCA directory

--- verdadeira GP TROCA/adicionar_coluna_email.sql
+++
@@ -1,7 +0,0 @@
--- SQL para adicionar a coluna email à tabela users
-ALTER TABLE users ADD COLUMN IF NOT EXISTS email TEXT;
-
--- SQL para verificar se a coluna foi adicionada
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'users' AND column_name = 'email'; 
--- verdadeira GP TROCA/src/components/Layout/BaseLayout.tsx
+++
@@ -1,4 +0,0 @@
-import { ConnectionStatus } from '../ConnectionStatus';
-
-// Dentro do componente BaseLayout, antes do <Container>
-<ConnectionStatus /> 
